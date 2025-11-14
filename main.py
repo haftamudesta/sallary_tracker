c989cd2832a7bf5ba76f08f604df3c874d6fa9d1
Haftamu Desta <haftamudesta>
2025-11-14 05:39:04 -0800

    Add name setter

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ class Employee:
         'mid-level': 3000,
         'senior': 4000,
     }
+
     def __init__(self, name, level):
         if not (isinstance(name, str) and isinstance(level, str)):
             raise TypeError("'name' and 'level' attribute must be of type 'str'.")
-        
+        if level not in Employee._base_salaries:
+            raise ValueError(f"Invalid value '{level}' for 'level' attribute.")
         self._name = name
         self._level = level
+        self._salary = Employee._base_salaries[level]
+
     def __str__(self):
         return f'{self.name}: {self.level}'
 
@@ -20,10 +24,20 @@ class Employee:
     @property
     def name(self):
         return self._name
-
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError("'name' must be a string.")
+        self._name = new_name
+        
     @property
     def level(self):
         return self._level
 
+    @property
+    def salary(self):
+        return self._salary
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
 print(charlie_brown)
+print(f'Base salary: ${charlie_brown.salary}')
